4064583163d1bd629f6fbfc21ac05db83a03266b
Maximilian <scientificresearch309@gmail.com>
2025-09-20 00:21:31 +0200

    and now all files are in and the Site is working totally well!

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,22 @@
 import type { Metadata } from 'next';
 import './globals.css';
 
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
 export const metadata: Metadata = {
   title: 'Tech Site',
 };
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} bg-green-100 p-6`}>{children}</body>
+    </html>
+  );
+}
