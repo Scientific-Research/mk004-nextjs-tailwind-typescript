1a6c4eaf0b516a3a9380461960ef262ecab039e2
Maximilian <scientificresearch309@gmail.com>
2025-09-20 14:26:38 +0200

    "use client" added

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,19 +1,37 @@
+'use client';
 import Link from 'next/link';
+import { useSelectedLayoutSegment } from 'next/navigation';
 
 export const Nav = () => {
+  const activeSegment = useSelectedLayoutSegment();
   return (
-    <nav className="bg-slate-600 text-white mb-4 rounded p-2 mainnav">
-      <ul className="flex gap-4 ml-1">
+    <nav>
+      <ul className="flex gap-4 bg-slate-700 text-white p-2 font-semibold rounded mb-4">
         <li>
-          <Link href="/">Welcome</Link>
+          <Link
+            href="/"
+            className={activeSegment === '(welcome)' ? 'text-yellow-200' : ''}
+          >
+            Welcome
+          </Link>
         </li>
 
         <li>
-          <Link href="/techskills">Tech Skills</Link>
+          <Link
+            href="/techskills"
+            className={activeSegment === 'techskills' ? 'text-yellow-200' : ''}
+          >
+            Tech Skills
+          </Link>
         </li>
 
         <li>
-          <Link href="/techbooks">Tech Books</Link>
+          <Link
+            href="/techbooks"
+            className={activeSegment === 'techbooks' ? 'text-yello-200' : ''}
+          >
+            Tech Books
+          </Link>
         </li>
       </ul>
     </nav>
